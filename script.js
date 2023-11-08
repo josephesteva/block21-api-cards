23bcd3f4dc7fa24ec1a1c3a5d9fa947884f22caf
josephesteva <joe.esteva@gmail.com>
2023-11-08 15:04:34 -0500

    Improve styling via both html and js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const state = {
 	recipes: []
 }
 
+const main = document.querySelector(`main`);
+
 const getRecipes = async () => {
 	const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/recipes`);
 	const recipesJSON = await response.json();
@@ -14,22 +16,31 @@ const getRecipes = async () => {
 } 
 
 const renderAllRecipes = (recipesArray) => {
-	const main = document.querySelector(`main`);
 	// const h2 = document.createElement(`h2`);
 	// h2.innerText = `Potential Recipes`;
-	main.appendChild(h2);
+	// main.appendChild(h2);
 	recipesArray.forEach(recipe => {
 		const section = document.createElement(`section`);
-		section.style = `width: 400px; height: 400px; border: solid black 3px`
+		section.style = `display: flex; align-items: center; flex-direction: column; width: 350px; height: 300px; border: solid black 3px; margin: 10px`
 		section.innerHTML = `
-		<h3>Recipe</h3>
-		<h4>${recipe.name}</h4>
-		<img src="${recipe.imageUrl}" alt="${recipe.name}" style="width: 300px;"/>
+		<h3>${recipe.name}</h3>
+		<img src="${recipe.imageUrl}" alt="${recipe.name}" style="width: 300px; height: 225px"/>
 		`
 		main.appendChild(section);
+
+		// section.addEventListener(`click`, () => {
+		// 	renderSingleRecipe();
+		// })
 	
 	});
 }
+
+// const renderSingleRecipe = async () => {
+	
+// 	main.innerHTML = `
+	
+// 	`
+// }
 
 
 getRecipes();
