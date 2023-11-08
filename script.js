12cd11369cc3d39e1926f636cc95d42800558c2f
josephesteva <joe.esteva@gmail.com>
2023-11-08 14:50:30 -0500

    Implement  initial getRecipes and renderAllRecipes functions

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 console.log(`hi`);
+
+const state = {
+	recipes: []
+}
+
+const getRecipes = async () => {
+	const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/recipes`);
+	const recipesJSON = await response.json();
+	state.recipes = recipesJSON.data;
+	console.log(state.recipes);
+
+	renderAllRecipes(state.recipes);
+} 
+
+const renderAllRecipes = (recipesArray) => {
+	const main = document.querySelector(`main`);
+	// const h2 = document.createElement(`h2`);
+	// h2.innerText = `Potential Recipes`;
+	main.appendChild(h2);
+	recipesArray.forEach(recipe => {
+		const section = document.createElement(`section`);
+		section.style = `width: 400px; height: 400px; border: solid black 3px`
+		section.innerHTML = `
+		<h3>Recipe</h3>
+		<h4>${recipe.name}</h4>
+		<img src="${recipe.imageUrl}" alt="${recipe.name}" style="width: 300px;"/>
+		`
+		main.appendChild(section);
+	
+	});
+}
+
+
+getRecipes();
